d895eccc75278678999796791775b68d7ad297ce
Aryan Lakshakar <144420659+AryanLakshakar-tech@users.noreply.github.com>
2026-03-26 09:25:19 +0530

    Sum_of_digits_of_a_number.cpp

---
+++ Loops/Sum_of_digits_of_a_number.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int dig_sum = 0;
+
+    while(n>0){
+       int lastdig = n%10;
+       dig_sum += lastdig;
+       n = n/10;
+    }
+
+    cout<< "Sum = " << dig_sum <<endl;
+    return 0;
+}
