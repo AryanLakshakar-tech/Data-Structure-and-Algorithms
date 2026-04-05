741449941fc51e9ecdacd9397fd8de3d791f0488
Aryan Lakshakar <144420659+AryanLakshakar-tech@users.noreply.github.com>
2026-04-05 11:15:40 +0530

    Create Max_subArray_sum._BF.cpp

---
+++ Arrays/Max_subArray_sum._BF.cpp
@@ -0,0 +1,29 @@
+//TC=O(n^3)
+#include<iostream>
+#include <climits>
+using namespace std;
+
+void maxSubarraySum(int *arr,int n){
+    int maxSum=INT_MIN;
+    
+    for(int start=0;start<n;start++){
+        for(int end=start;end<n;end++){
+            int currsum=0;
+            for(int i=start;i<=end;i++){
+                currsum+=arr[i];
+            }
+            cout<<currsum<<",";
+            maxSum=max(maxSum,currsum);
+        }
+        cout<<endl;
+    }
+    cout<<"Maximum subarray sum = "<<maxSum<<endl;
+
+}
+int main(){
+    int arr[]={2,-3,6,-5,4,2};
+    int n=sizeof(arr)/sizeof(int);
+
+    maxSubarraySum(arr,n);
+    return 0;
+}
