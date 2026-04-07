7ae3de507dfccb2ce8322283be9a2e703787dedc
Aryan Lakshakar <144420659+AryanLakshakar-tech@users.noreply.github.com>
2026-04-07 20:14:29 +0530

    Create 42. Trapping_Rain_Water.cpp

---
+++ Leetcode/Array/42. Trapping_Rain_Water.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n=height.size();
+        int lmax=height[0];
+        int rmax=height[n-1];
+
+        int low=1;
+        int high=n-2;
+        int ans=0;
+
+        while(low<=high){
+            lmax=max(lmax,height[low]);
+            rmax=max(rmax,height[high]);
+
+            if(lmax<rmax){
+                ans+=lmax-height[low];
+                low++;
+            }
+
+            else{
+                ans+=rmax-height[high];
+                high--;
+            }
+        }
+        return ans;
+        
+    }
+};
