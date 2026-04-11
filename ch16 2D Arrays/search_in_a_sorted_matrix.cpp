ddfcdb93bc4e72c33a641795486d432c62f90060
Aryan Lakshakar <144420659+AryanLakshakar-tech@users.noreply.github.com>
2026-04-11 20:37:50 +0530

    Add files via upload

---
+++ ch16 2D Arrays/search_in_a_sorted_matrix.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+bool search(int mat[][4],int n,int m,int key){
+    int i=0,j=m-1;
+
+    while(i<n && j>=0){
+        if(mat[i][j]==key){
+            cout<<"Found at cell ("<<i<<","<<j<<") \n";
+            return true;
+        }else if(mat[i][j]>key){
+            //left
+            j--;
+        }else{
+            //down
+            i++;
+        }
+    }
+    cout<<"Key not found "<<endl;
+    return false;
+}
+
+
+int main() {
+
+    int n=4,m=4,key=12;
+    int mat[4][4]={{1,2,3,4},
+                   {5,6,7,8},
+                   {9,10,11,12},
+                   {13,14,15,16}};
+
+    search(mat,n,m,key);
+    return 0;
+}
