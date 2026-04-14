44a77ab195ae1dc1c683ccddbb70516441948a41
Aryan Lakshakar <144420659+AryanLakshakar-tech@users.noreply.github.com>
2026-04-14 16:33:54 +0530

    Create 867. Transpose_Matrix.cpp

---
+++ Leetcode/Array/867. Transpose_Matrix.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        int n=matrix.size(); //rows
+        int m=matrix[0].size();  //columns
+
+        //create a result matrix of size m,n
+        vector<vector<int>> result(m,vector<int>(n));
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                result[j][i]=matrix[i][j];
+            }
+        }
+
+        return result;
+
+        
+    }
+};
