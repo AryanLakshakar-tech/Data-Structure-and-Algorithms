b6ac32af8d0eee1ba18188c156c47c9fa5c372c1
Aryan Lakshakar <144420659+AryanLakshakar-tech@users.noreply.github.com>
2026-04-19 20:16:53 +0530

    Create 1436. Destination_City.cpp

---
+++ Leetcode/Strings/1436. Destination_City.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string destCity(vector<vector<string>>& paths) {
+        unordered_set<string> st;
+
+        for(auto &path : paths){
+            string source = path[0];
+
+            st.insert(source); //outgoing edge;
+
+        }
+        
+        for(auto &path : paths){
+            string destination = path[1];
+
+            if(st.find(destination)==st.end()){
+                return destination;
+            }
+
+        } 
+        return "";
+    }
+
+};
