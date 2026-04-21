5a9b9d334ed1a02c3e18bd5f496f3a54a93766f2
Aryan Lakshakar <144420659+AryanLakshakar-tech@users.noreply.github.com>
2026-04-21 07:30:16 +0530

    Create 242. Valid_Anagram.cpp

---
+++ Leetcode/Strings/242. Valid_Anagram.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        vector<int> count(26,0);
+
+        for(char &ch : s){
+            count[ch-'a']++;
+        }
+        
+        for(char &ch : t){
+            count[ch-'a']--;
+        }
+
+        //check all elements in count is zero
+        //for loop
+
+        bool allzeros = all_of(begin(count), end(count),[](int element){
+            return element==0;
+        });
+
+        return allzeros;
+    }
+};
