01164e96ede10d6f64554f7a2eb0b3b1628433a5
Aryan Lakshakar <144420659+AryanLakshakar-tech@users.noreply.github.com>
2026-05-09 21:14:39 +0530

    Update 121. Best_time_to_buy_and_sell_stock.cpp

--- Leetcode/Array/121. Best_time_to_buy_and_sell_stock.cpp
+++ Leetcode/Array/121. Best_time_to_buy_and_sell_stock.cpp
@@ -20,3 +20,25 @@ public:
         
     }
 };
+
+
+/*  Optimized solution
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+
+        int minPrice = INT_MAX;
+        int maxProfit = 0;
+
+        for (int price : prices) {
+
+            minPrice = min(minPrice, price);
+
+            maxProfit = max(maxProfit, price - minPrice);
+        }
+
+        return maxProfit;
+    }
+};
+*/
