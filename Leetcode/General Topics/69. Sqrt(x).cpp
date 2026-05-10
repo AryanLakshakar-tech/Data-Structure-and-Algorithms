41f8cefa9a0beb90526094bb1c90c2bb1cde1baa
Aryan Lakshakar <144420659+AryanLakshakar-tech@users.noreply.github.com>
2026-05-10 21:34:07 +0530

    Update 69. Sqrt(x).cpp

--- Leetcode/General Topics/69. Sqrt(x).cpp
+++ Leetcode/General Topics/69. Sqrt(x).cpp
@@ -20,3 +20,30 @@ public:
         
     }
 };
+
+/*
+class Solution {
+public:
+    int mySqrt(int x) {
+
+        long long low = 0, high = x;
+        long long ans = 0;
+
+        while (low <= high) {
+
+            long long mid = low + (high - low) / 2;
+
+            if (mid * mid <= x) {
+                ans = mid;
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+};
+
+*/
