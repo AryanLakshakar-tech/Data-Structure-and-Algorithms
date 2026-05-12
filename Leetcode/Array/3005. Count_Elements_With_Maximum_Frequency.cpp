299659dcefe0e01a87bb32443808be1fd8a7fc0e
Aryan Lakshakar <144420659+AryanLakshakar-tech@users.noreply.github.com>
2026-05-12 12:41:59 +0530

    Update 3005. Count_Elements_With_Maximum_Frequency.cpp

--- Leetcode/Array/3005. Count_Elements_With_Maximum_Frequency.cpp
+++ Leetcode/Array/3005. Count_Elements_With_Maximum_Frequency.cpp
@@ -16,3 +16,29 @@ public:
         
     }
 };
+
+//Approach 2
+class Solution {
+public:
+    int maxFrequencyElements(vector<int>& nums) {
+        // using 1 pass
+        vector<int> Count(101);   //we can also use unordered_map<int, int> mp;
+
+        int maxfreq=0;
+        int total=0;
+
+        for(int &num:nums){
+            Count[num]++;
+
+            int freq=Count[num];
+
+            if(freq>maxfreq){
+                maxfreq=freq;   
+                total=maxfreq;
+            }else if(freq==maxfreq){
+                total+=maxfreq;
+            }
+        }
+        return total;
+    }
+};
